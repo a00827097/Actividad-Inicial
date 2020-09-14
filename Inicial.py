9ba90741ca9fe3d10dccf093932288e91f1dd70f
Emma Gómez Guerra <70240999+a00827097@users.noreply.github.com>
2020-09-14 11:24:26 -0500

    Add files via upload

---
+++ Inicial.py
@@ -0,0 +1,25 @@
+#A00827097 Emma Gomez Guerra
+#A01633320 José Diego S. LArragiti
+#proposito del programa
+import math
+
+#funcion1  calcular área del rectángulo
+def area_rectangulo(l1,l2):
+    area = l1*l2
+    return print('area del rectangulo es: ', area)
+
+#funcion2  calcular perímetro del rectángulo
+
+
+#instrucciones de accion
+#pedir datos
+print("medida de un lado del rectángulo")
+l1 = float(input())
+
+print("medida de un lado del rectángulo")
+l2 = float(input())
+
+#desplegar calculo funcion1
+
+#desplegar calculo funcion 2
+
